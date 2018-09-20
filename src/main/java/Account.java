ea55101a067fbaa6870afc6f0b44d5b026c2d5f9
kanokponth <kanokpon.th@ku.th>
2018-09-20 22:24:57 +0700

    Initial commit

---
+++ src/main/java/Account.java
@@ -0,0 +1,106 @@
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class Account {
+    private SimpleStringProperty id;
+    private SimpleStringProperty day;
+    private SimpleStringProperty cata;
+    private SimpleStringProperty description;
+    private SimpleStringProperty showMoney;
+    private SimpleDoubleProperty money;
+    private String input;
+    private Double balance=0.0;
+    private int newId;
+
+    public int getNewId() {
+        return newId;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public String getInput() {
+        return input;
+    }
+
+    public void setInput(String input) {
+        this.input = input;
+    }
+
+    public Account() {
+    }
+
+    public String getShowMoney() {
+        return showMoney.get();
+    }
+
+
+    public void setShowMoney(String showMoney) {
+        this.showMoney.set(showMoney);
+    }
+
+    public Account(String id, String day, String cata, String description,String show, Double money) {
+        this.id = new SimpleStringProperty(id);
+        this.day = new SimpleStringProperty(day);
+        this.cata = new SimpleStringProperty(cata);
+        this.description = new SimpleStringProperty(description);
+        this.showMoney = new SimpleStringProperty(show);
+        this.money =new SimpleDoubleProperty(money);
+    }
+
+    public String getId() {
+        return id.get();
+    }
+
+    public void setId(String id) {
+        this.id.set(id);
+    }
+    public String getDay() {
+        return day.get();
+    }
+    public void setDay(String day) {
+        this.day.set(day);
+    }
+    public String getCata() {
+        return cata.get();
+    }
+    public void setCata(String cata) {
+        this.cata.set(cata);
+    }
+
+    public String getDescription() {
+        return description.get();
+    }
+    public void setDescription(String description) {
+        this.description.set(description);
+    }
+    public double getMoney() {
+        return money.get();
+    }
+    public void setMoney(double money) {
+        this.money.set(money);
+    }
+
+    public Double balanceMoney(String type,Double money){
+        if(type.equals("+")){
+            balance+=money;
+        }
+        else if(type.equals("-")){
+            balance-=money;
+        }
+        return balance;
+    }
+    public int addId(int id){
+        newId=id;
+        return newId; }
+
+
+}
